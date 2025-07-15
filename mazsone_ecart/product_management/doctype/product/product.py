72a7a24bfa576b349a01c22dc7044bba2dfc6761
saranR20s <720721106096@hicet.ac.in>
2025-07-15 11:38:15 +0000

    work in product management module for seo and category backend

--- mazsone_ecart/product_management/doctype/product/product.py
+++ mazsone_ecart/product_management/doctype/product/product.py
@@ -3,7 +3,20 @@
 
 # import frappe
 from frappe.model.document import Document
+import frappe  # Import the Frappe module
+import re
+from frappe.utils import cstr
 
+def slugify(text):
+    text = cstr(text).lower()
+    text = re.sub(r'[^\w\s-]', '', text)       # Remove special chars
+    text = re.sub(r'[\s_-]+', '-', text)       # Replace space/underscore with dash
+    return text.strip('-')
 
 class Product(Document):
-	pass
+    def autoname(self):
+        if not self.slug:
+            self.slug = slugify(self.item_name)
+
+
+ 
